1b71cc704b318a6b8575a7818a355b7cbff29b62
Gregory Knight <gregory.knight@vocalocity.com>
2016-06-20 14:08:07 -0400

    testing fix

--- site/profiles/manifests/panopuppet.pp
+++ site/profiles/manifests/panopuppet.pp
@@ -4,6 +4,10 @@
 class profiles::panopuppet {
 
 
-include panopuppet
+#include panopuppet
+#Changed to use $puppetdb_url , which should be set outside of manifest for testing!
+  class { 'panopuppet':
+    puppetdb_url => $puppetdb_url,
+  }
 
 }
