1ae6a47af7176f717f42065721d53358ca3e50b6
Gregory Knight <gregory.knight@vocalocity.com>
2016-06-20 14:54:50 -0400

    testing

--- site/profiles/manifests/panopuppet.pp
+++ site/profiles/manifests/panopuppet.pp
@@ -14,27 +14,27 @@ $wsgi_thread_count = 5
     default_vhost => false,
   }
 
-  apache::vhost { 'panopuppet':
-      docroot             => $static_root,
-      port                => 80,
-      wsgi_script_aliases => { '/' => "${wsgi_dir}/wsgi.py" },
-      wsgi_daemon_process => "panopuppet",
-
-      wsgi_daemon_process_options => {
-        threads => $wsgi_thread_count,
-      },
-
-      aliases => [{
-        alias => '/static',
-        path  => $static_root,
-      }],
-
-      directories => [{
-        'path'    => $static_root,
-        'require' => 'all granted',
-      }],
-    require => Class['apache'],
-  }
+#  apache::vhost { 'panopuppet':
+#      docroot             => $static_root,
+#      port                => 80,
+#      wsgi_script_aliases => { '/' => "${wsgi_dir}/wsgi.py" },
+#      wsgi_daemon_process => "panopuppet",
+#
+#      wsgi_daemon_process_options => {
+#        threads => $wsgi_thread_count,
+#      },
+#
+#      aliases => [{
+#        alias => '/static',
+#        path  => $static_root,
+#      }],
+#
+#      directories => [{
+#        'path'    => $static_root,
+#        'require' => 'all granted',
+#      }],
+#    require => Class['apache'],
+#  }
 
 
 }
