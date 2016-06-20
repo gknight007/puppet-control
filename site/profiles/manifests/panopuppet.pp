1f6eafeba3901d2be551002a32604b13c765a9ea
Gregory Knight <gregory.knight@vocalocity.com>
2016-06-20 14:41:18 -0400

    refactor

--- site/profiles/manifests/panopuppet.pp
+++ site/profiles/manifests/panopuppet.pp
@@ -3,15 +3,33 @@
 
 class profiles::panopuppet {
 
+$static_root = '/var/www/panopuppet/staticfiles'
+$wsgi_dir = '/var/www/panopuppet'
+$wsgi_thread_count = 5
 
 #include panopuppet
-#Changed to use $puppetdb_url , which should be set outside of manifest for testing!
-  class { 'panopuppet':
-    puppetdb_url => $puppetdb_url,
+  include panopuppet
+
+  apache::vhost { 'panopuppet':
+      docroot             => $static_root,
+      port                => 80,
+      wsgi_script_aliases => { '/' => "${wsgi_dir}/wsgi.py" },
+      wsgi_daemon_process => "panopuppet",
+
+      wsgi_daemon_process_options => {
+        threads => $wsgi_thread_count,
+      },
+
+      aliases => [{
+        alias => '/static',
+        path  => $static_root,
+      }],
+
+      directories => [{
+        'path'    => $static_root,
+        'require' => 'all granted',
+      }],
   }
 
-  class { 'apache':
-    default_vhost => false,
-  }
 
 }
